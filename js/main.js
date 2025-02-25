1f10224a1f32f11da8d2ea24f59df66c44874e83
DmitryMVP <masqueunclub17@gmail.com>
2025-02-25 22:24:10 +0500

    Add Swiper

---
+++ js/main.js
@@ -0,0 +1,55 @@
+
+let swiper = null;
+
+window.addEventListener('resize', function () {
+    const currentInnerWidth = window.innerWidth;
+
+    if (currentInnerWidth < 768) {
+
+        if (!swiper) {
+            const swiperElement = document.querySelector('.swiper');
+
+            const observer = new ResizeObserver(() => {
+                const widthSwiper = swiperElement.offsetWidth;
+                if (swiper) {
+                    swiper.params.slidesPerView = widthSwiper / 267;
+                    swiper.update();
+                }
+            });
+
+            observer.observe(swiperElement);
+
+            swiper = new Swiper('.swiper', {
+                direction: 'horizontal',
+                loop: true,
+                spaceBetween: 16,
+                pagination: {
+                    el: '.swiper-pagination',
+                    clickable: true,
+                },
+                slidesPerView: 1,
+            });
+        }
+        
+    } else {
+        if (swiper) {
+            swiper.destroy(true, true);
+            swiper = null;
+        }
+    }
+});
+
+let brandsMoreElement = document.querySelector('#brands__more');
+let containerElement = document.querySelector('.container');
+let moreElement = document.querySelector('.more');
+
+
+brandsMoreElement.addEventListener('click', function () {
+    containerElement.classList.toggle('container--open');
+    moreElement.classList.toggle('more--active');
+    if (containerElement.classList.contains('container--open')) {
+        brandsMoreElement.textContent = 'Скрыть';
+    } else {
+        brandsMoreElement.textContent = 'Показать все';
+    }
+});
